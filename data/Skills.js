eacb2bedd84010962aca55d635476ef99507eae7
ousseini <adnanejiko@gmail.com>
2022-10-08 19:13:20 +0000

    create data for skills

---
+++ data/Skills.js
@@ -0,0 +1,52 @@
+const Skills = [
+  {
+    id: 'HTML',
+    img: './images/skills/html5.svg',
+  },
+  {
+    id: 'CSS',
+    img: './images/skills/css3.svg',
+  },
+  {
+    id: 'SASS',
+    img: './images/skills/sass.svg',
+  },
+  {
+    id: 'JAVASCRIPT',
+    img: './images/skills/javascript.svg',
+  },
+  {
+    id: 'BOOTSTRAP',
+    img: './images/skills/bootstrap.svg',
+  },
+  {
+    id: 'REACT JS',
+    img: './images/skills/react.svg',
+  },
+  {
+    id: 'REDUX',
+    img: './images/skills/redux.svg',
+  },
+  {
+    id: 'NEXT JS',
+    img: './images/skills/next-js.svg',
+  },
+  {
+    id: "DART",
+    img: "./images/skills/dart.svg"
+  },
+  {
+    id: "FLUTTER",
+    img: "./images/skills/flutter.svg"
+  },
+  {
+    id: "C#",
+    img: "./images/skills/csharp.svg"
+  },
+  {
+    id: "C#",
+    img: "./images/skills/net-core.svg"
+  },
+];
+
+export default Skills;
